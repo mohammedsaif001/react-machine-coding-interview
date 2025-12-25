57a07d3f261182ba68f8cf9859829822d649fe3f
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-25 16:15:33 +0000

    Counter FUnction using Class Based Component

--- src/1_class_based_comp/class-based-comp.jsx
+++ src/1_class_based_comp/class-based-comp.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 class ClassBasedComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prev) => ({
+      ...prev,
+      counter: prev.counter + 1,
+    }));
+  };
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <div>Counter: {this.state.counter}</div>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
   }
 }
 
