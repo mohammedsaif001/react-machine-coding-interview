9eac040f81854031f38718cb3028b1cd062dc6bc
Mohammed Saif <88727111+mohammedsaif001@users.noreply.github.com>
2025-12-30 15:10:58 +0000

    Renaming Folders

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import ProgressBarView from "./12_progress_bar/progress-bar-view.jsx";
 import PaginationBackendView from "./13_pagination/pagination-backend-view.jsx";
 import PaginationView from "./13_pagination/pagination-view.jsx";
 import InfiniteScrollView from "./14_infinite_scroll/infinite-scroll-view.jsx";
-import ClassBasedComp from "./1_class_based_comp/class-based-comp";
-import LifeCycleClassComp from "./1_class_based_comp/life-cycle-class-comp.jsx";
-import UseEffectPolyfillComponent from "./2_use_effect_polyfill/use-effect-polyfill.jsx";
-import LightDarkMode from "./3_context_light_dark_mode/light-dark-mode.js";
-import UseReducerComponent from "./4_use_reducer/use-reducer-comp.jsx";
-import UseMemoCallbackComp from "./5_use_memo_callback/use-memo-callback-comp.jsx";
-import UseImperativeHandleComponent from "./6_use_imperative_handle/use-imperative-handle-comp.jsx";
-import UseWindowSize from "./7_use_window/use-window-size.jsx";
-import UseDebounceHook from "./8_use_debounce/use-debounce-comp.jsx";
+import ClassBasedComp from "./01_class_based_comp/class-based-comp";
+import LifeCycleClassComp from "./01_class_based_comp/life-cycle-class-comp.jsx";
+import UseEffectPolyfillComponent from "./02_use_effect_polyfill/use-effect-polyfill.jsx";
+import LightDarkMode from "./03_context_light_dark_mode/light-dark-mode.js";
+import UseReducerComponent from "./04_use_reducer/use-reducer-comp.jsx";
+import UseMemoCallbackComp from "./05_use_memo_callback/use-memo-callback-comp.jsx";
+import UseImperativeHandleComponent from "./06_use_imperative_handle/use-imperative-handle-comp.jsx";
+import UseWindowSize from "./07_use_window/use-window-size.jsx";
+import UseDebounceHook from "./08_use_debounce/use-debounce-comp.jsx";
 import "./styles.css";
 
 export default function App() {
